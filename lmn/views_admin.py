bc79b62d184a30170c23cf5c9e13f2365c566bf6
Lane Enget <lane.enget@go.minneapolis.edu>
2020-05-07 13:55:35 -0500

    Fix views_admin.py to save more artists, venues, shows in database

--- lmn/views_admin.py
+++ lmn/views_admin.py
@@ -8,7 +8,8 @@ import os
 def get_shows():
 
     key = os.environ.get('TICK_MASTER')
-    query = {'apikey': key}
+    c_name = 'music'
+    query = {'classificationName': c_name, 'apikey': key}
     url = 'https://app.ticketmaster.com/discovery/v2/events.json?'
     request = requests.get(url, params=query)
     response = request.json()
@@ -19,40 +20,42 @@ def get_shows():
     
     request.raise_for_status()
 
-def create_artist(response):
+def create_artist(response, i):
 
     if response is not None:
         try:
-            artist_name = response['_embedded']['events'][0]['name']
+            artist_name = response['_embedded']['events'][i]['name']
             artist = Artist(name=artist_name).save()
             return artist
         except IntegrityError:
             pass
 
-def create_venue(response):
+def create_venue(response, i):
 
     if response is not None:    
         try:
-            venue_name = response['_embedded']['events'][0]['_embedded']['venues'][0]['name']
-            city = response['_embedded']['events'][0]['_embedded']['venues'][0]['city']['name']
-            state = response['_embedded']['events'][0]['_embedded']['venues'][0]['state']['name']
+            venue_name = response['_embedded']['events'][i]['_embedded']['venues'][0]['name']
+            city = response['_embedded']['events'][i]['_embedded']['venues'][0]['city']['name']
+            state = response['_embedded']['events'][i]['_embedded']['venues'][0]['state']['name']
             venue = Venue(name=venue_name, city=city, state=state).save()
             return venue
         except IntegrityError:
            pass
 
-def create_show(response, artist, venue):
+def create_show(response, artist, venue, i):
 
     if response is not None:
         try:
-            show_date = response['_embedded']['events'][0]['dates']['start']['dateTime']
+            show_date = response['_embedded']['events'][i]['dates']['start']['dateTime']
             show = Show(show_date=show_date, artist=artist, venue=venue).save()
+            return show
         except IntegrityError:
             pass
 
 def admin_main(request):
     response = get_shows()
-    artist = create_artist(response)
-    venue = create_venue(response)
-    create_show(response, artist, venue)
+    for i in range(0, 10):
+        artist = create_artist(response,  i)
+        venue = create_venue(response, i)
+        create_show(response, artist, venue, i)
     return HttpResponse('ok')

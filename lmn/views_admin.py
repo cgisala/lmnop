7642397436c1d068172cddfd9513607d1a7f52c0
Jordan Gerdin <jordan.gerdin@gmail.com>
2020-05-08 17:47:18 -0600

    Fixed API data to create shows, expanded size of requests to 100

--- lmn/views_admin.py
+++ lmn/views_admin.py
@@ -2,14 +2,19 @@ import requests
 from .models import Artist, Venue, Show
 from django.http import HttpResponse
 from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 import os
 
 
 def get_shows():
 
+    # Parameters for API request
     key = os.environ.get('TICK_MASTER')
-    c_name = 'music'
-    query = {'classificationName': c_name, 'apikey': key}
+    c_name = 'music'    # Filter events to music
+    num_shows = 200     # Number of upcoming shows
+    countryCode = 'US'
+    
+    query = {'classificationName': c_name, 'countryCode': countryCode, 'size':num_shows, 'apikey': key}
     url = 'https://app.ticketmaster.com/discovery/v2/events.json?'
     request = requests.get(url, params=query)
     response = request.json()
@@ -22,39 +27,76 @@ def get_shows():
 
 def create_artist(response, i):
 
-    if response is not None:
+    if response is not None:        
+        # Get artist name from response
+        artist_name = response['_embedded']['events'][i]['name']
+
         try:
-            artist_name = response['_embedded']['events'][i]['name']
-            artist = Artist(name=artist_name).save()
-            return artist
+            # Check if data is in database already
+            artist = Artist.objects.get(name=artist_name)
+        
+        except ObjectDoesNotExist:
+            # If artist isn't in database, create
+            artist = Artist(name=artist_name)
+            artist.save()
+            
         except IntegrityError:
             pass
+            
+        return artist
 
 def create_venue(response, i):
 
-    if response is not None:    
+    if response is not None:   
+        # Get venue details from response
+        venue_name = response['_embedded']['events'][i]['_embedded']['venues'][0]['name']
+        city = response['_embedded']['events'][i]['_embedded']['venues'][0]['city']['name']
+        state = response['_embedded']['events'][i]['_embedded']['venues'][0]['state']['name']
+
         try:
-            venue_name = response['_embedded']['events'][i]['_embedded']['venues'][0]['name']
-            city = response['_embedded']['events'][i]['_embedded']['venues'][0]['city']['name']
-            state = response['_embedded']['events'][i]['_embedded']['venues'][0]['state']['name']
-            venue = Venue(name=venue_name, city=city, state=state).save()
-            return venue
+            # Check if data is in database already
+            venue = Venue.objects.get(name=venue_name, city=city, state=state)
+
+        except ObjectDoesNotExist:
+            # If venue isn't in database, create
+            venue = Venue(name=venue_name, city=city, state=state)    
+            venue.save()
+        
         except IntegrityError:
-           pass
+            pass
+        
+        return venue
 
 def create_show(response, artist, venue, i):
 
     if response is not None:
-        try:
-            show_date = response['_embedded']['events'][i]['dates']['start']['dateTime']
-            show = Show(show_date=show_date, artist=artist, venue=venue).save()
-            return show
+        show_date_time = response['_embedded']['events'][i]['dates']['start']
+        
+        # Check if scheduled date is TBA or no specific time, if either ignore
+        if show_date_time['timeTBA']:
+            return
+        if show_date_time['noSpecificTime']:
+            return
+        # If show has valid dateTime field, create show
+        if show_date_time['dateTime']:
+            show_date = show_date_time['dateTime']
+        else:
+            return
+
+        try:    
+            show = Show.objects.get(show_date=show_date, artist=artist, venue=venue)
+
+        except ObjectDoesNotExist:
+            show = Show(show_date=show_date, artist=artist, venue=venue)
+            show.save()
+
         except IntegrityError:
             pass
 
 def admin_main(request):
     response = get_shows()
-    for i in range(0, 10):
+
+    for i in range(0, len(response['_embedded']['events'])):
         artist = create_artist(response,  i)
         venue = create_venue(response, i)
         create_show(response, artist, venue, i)
